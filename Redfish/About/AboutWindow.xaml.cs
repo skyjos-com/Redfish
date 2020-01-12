7f95514c1c1d87717206522d6807fd5c7efe8f18
stevendev <steven.jane.zhang@gmail.com>
2020-01-12 19:56:23 +0800

    update About window

--- Redfish/About/AboutWindow.xaml.cs
+++ Redfish/About/AboutWindow.xaml.cs
@@ -24,10 +24,17 @@ namespace Redfish.About
             InitializeComponent();
         }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.description_textblock.Text = "Redfish is a simple and flexible SMB server on Windows. It's maintained by Skyjos team and open source on GitHub";
+        }
+
         private void Hyperlink_Website(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
         {
             Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
             e.Handled = true;
         }
+
+        
     }
 }
